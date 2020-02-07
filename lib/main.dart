6d53315bc138908a96d8e564933b82a3021ec2b5
Orlando Hernandez <oaraz02@gmail.com>
2020-02-07 08:30:01 -0500

    sudoku board

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sudoku_solver/src/pages/home_page.dart';
+import 'package:sudoku_solver/src/pages/sudoku_page.dart';
 
 void main() => runApp(SudokuSolver());
 
@@ -8,10 +9,11 @@ class SudokuSolver extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Sudoku solver',
-        initialRoute: 'home',
+        initialRoute: 'sudoku',
         debugShowCheckedModeBanner: false,
         routes: {
-          'home': (BuildContext context) => HomePage()
+          'home': (BuildContext context) => HomePage(),
+          'sudoku': (BuildContext context) => SudokuPage()
         });
   }
 }
---
+++ lib/src/pages/sudoku_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
+
+class SudokuPage extends StatefulWidget {
+  @override
+  _SudokuPageState createState() => _SudokuPageState();
+}
+
+class _SudokuPageState extends State<SudokuPage> {
+  List<List<int>> _sudokuGrid;
+  Tuple2<int, int> _selectedCell = Tuple2<int, int>(0, 0);
+
+  @override
+  void initState() {
+    super.initState();
+    _sudokuGrid = [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sudoku solver'),
+        actions: <Widget>[],
+      ),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 9,
+            children: List.generate(81, (index) {
+              int x, y = 0;
+              x = (index / 9).floor();
+              y = (index % 9);
+
+              return InkWell(
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 0.2),
+                      color: this._selectedCell.item1 == x &&
+                              this._selectedCell.item2 == y
+                          ? Colors.grey
+                          : Colors.white),
+                  child: Center(
+                    child: Text(
+                      '${this._sudokuGrid[x][y]}',
+                      style: Theme.of(context).textTheme.headline,
+                    ),
+                  ),
+                ),
+                onTap: () {
+                  this.setState(() {
+                    this._selectedCell = Tuple2<int, int>(x, y);
+                  });
+                },
+              );
+            }),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 9,
+            children: List.generate(9, (index) {
+              return InkWell(
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      '${index % 10}',
+                      style: Theme.of(context).textTheme.headline,
+                    ),
+                  ),
+                ),
+                onTap: () {
+                  this.setState(() {
+                    this._sudokuGrid[this._selectedCell.item1]
+                        [this._selectedCell.item2] = index % 10;
+                  });
+                },
+              );
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+}
